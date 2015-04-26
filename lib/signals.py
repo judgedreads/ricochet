ee662c25d3d34ce931e39754aa8ca3a2cded3324
judgedreads <pearce@millerdedmon.com>
2015-04-26 11:46:12 -0700

    set player for server

--- lib/signals.py
+++ lib/signals.py
@@ -5,9 +5,10 @@ import subprocess
 
 class Server(object):
 
-    def __init__(self):
+    def __init__(self, player):
         # create a named pipe for communication
         self.server = socket(AF_UNIX, SOCK_DGRAM)
+        self.player = player
         try:
             self.server.bind('/tmp/ricochet')
         except OSError:
@@ -21,24 +22,24 @@ class Server(object):
         sock = socket(AF_UNIX, SOCK_DGRAM)
         sock.connect('/tmp/ricochetctl')
         if data == "toggle":
-            player.toggle(None)
+            self.player.toggle(None)
             message = "toggle"
         elif data == "next":
-            player.skip_next(None)
+            self.player.skip_next(None)
             message = "next"
         elif data == "prev":
-            player.skip_prev(None)
+            self.player.skip_prev(None)
             message = "prev"
         elif data == "pos":
-            pos_min, pos_sec, dur_min, dur_sec = player.get_info(
+            pos_min, pos_sec, dur_min, dur_sec = self.player.get_info(
                 None, data)
             message = "%d:%d/%d:%d" % (pos_min, pos_sec, dur_min, dur_sec)
         elif data == "artist":
-            message = player.get_info(None, data)
+            message = self.player.get_info(None, data)
         elif data == "album":
-            message = player.get_info(None, data)
+            message = self.player.get_info(None, data)
         elif data == "song":
-            message = player.get_info(None, data)
+            message = self.player.get_info(None, data)
 
         info = bytes(message, 'UTF-8')
         sock.sendall(info)
