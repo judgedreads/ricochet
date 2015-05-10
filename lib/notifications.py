89622241a6a233d88d8a0787eb941bbd3ad1d981
judgedreads <pearce@millerdedmon.com>
2015-05-09 19:11:46 -0700

    tidy: remove extensions, use program icon, better body text

--- lib/notifications.py
+++ lib/notifications.py
@@ -24,12 +24,22 @@ class Notifier(object):
         artist = self.playlist[i].split('/')[-3]
         cover = "/home/judgedreads/Music/" + \
             artist + '/' + album + '/' + 'cover.jpg'
+        icon = '/opt/ricochet/images/ricochet.png'
 
-        Playing = Notify.Notification.new(song, artist, cover)
+        title = ''.join(song.split('.')[0:-1])
+        body = 'by %s\non %s' % (artist, album)
+
+        Playing = Notify.Notification.new(title, body, icon)
         Playing.add_action('action', 'Next', self.notif_skip, 'next')
         image = GdkPixbuf.Pixbuf.new_from_file(cover)
         Playing.set_image_from_pixbuf(image)
         Playing.show()
+
+        # TODO: make dict for each song (probably in browser), where key should
+        # be readable name and val should be dict of info containing file path,
+        # album, cover art, artist, etc. Then can put name on treeview and then
+        # pass corresponding dict through to everything. Need to have some
+        # cleanup methods to get rid of this stuff when it's done.
 
     def notif_skip(self, notif, action, data):
         if data == 'next':
