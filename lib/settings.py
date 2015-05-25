753d4ff06984b07be40b598f91e1c3bd529dfd31
judgedreads <pearce@millerdedmon.com>
2015-05-24 23:14:49 -0700

    remove username from settings defaults

--- lib/settings.py
+++ lib/settings.py
@@ -1,8 +1,16 @@
 import os
 
 # set up defaults
-settings = {'backend': 'gstreamer', 'mpd_host': 'localhost', 'mpd_port': 6600, 'music_dir': '/home/judgedreads/Music/',
-            'grid_icon_size': 128, 'detail_icon_size': 256, 'notifications': 'True', 'system_tray': 'True'}
+settings = {
+    'backend': 'gstreamer',
+    'mpd_host': 'localhost',
+    'mpd_port': 6600,
+    'music_dir': '%s/Music/' % os.environ['HOME'],
+    'grid_icon_size': 128,
+    'detail_icon_size': 256,
+    'notifications': 'True',
+    'system_tray': 'False'
+}
 
 home = os.environ['HOME']
 path = home + '/.config/ricochet/ricochetrc'
