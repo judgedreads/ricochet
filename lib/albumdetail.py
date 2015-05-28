a35487e44e8a1e3fe14044d9f04f993a80e32f7a
judgedreads <pearce@millerdedmon.com>
2015-05-27 22:01:44 -0700

    only use tabs for multiple discs

--- lib/albumdetail.py
+++ lib/albumdetail.py
@@ -12,7 +12,7 @@ class Album(Gtk.Window):
 
     def __init__(self, name, player):
         Gtk.Window.__init__(self, title=name)
-        self.set_default_size(256, 512)
+        self.set_default_size(300, 600)
 
         self.name = name
         self.player = player
@@ -38,51 +38,60 @@ class Album(Gtk.Window):
             if os.path.isdir(os.path.join(music, name, item)):
                 discs.append(os.path.join(name, item))
         if discs == []:
-            discs.append(name)
+            tracklist = self.get_tracklist(name)
+        else:
+            tracklist = self.multi_disc(discs)
 
+        vbox.pack_start(tracklist, True, True, 0)
+
+        self.show_all()
+
+    def multi_disc(self, discs):
         discs.sort()
         tabbed = Gtk.Notebook()
         tabbed.set_scrollable(True)
         for disc in discs:
-
-            # scrolled area for the songs
-            scroll = Gtk.ScrolledWindow()
-            scroll.set_border_width(0)
-            scroll.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
-
-            # set up the song treeview
-            liststore = Gtk.ListStore(str)
-            songs = os.listdir(music + disc)
-            songs.sort()
-            for song in songs:
-                if song.endswith(("mp3", "mpc", "ogg", "wma", "m4a", "mp4", "flac")):
-                    liststore.append([song])
-            treeview = Gtk.TreeView(model=liststore)
-            renderer = Gtk.CellRendererText()
-            # option is PangoEllipsizeMode numbered 0,1,2,3 for none, start,
-            # middle, end (True becomes 1=start)
-            renderer.set_property("ellipsize", 3)
-            column = Gtk.TreeViewColumn("Track", renderer, text=0)
-            treeview.append_column(column)
-            treeview.set_property("headers-visible", False)
-            # disable search grabbing keyboard input
-            treeview.set_enable_search(False)
-
-            # allow selection of multiple rows. get_selection won't work
-            # now so use get_selected_rows instead (on selection object)
-            Gtk.TreeSelection.set_mode(
-                treeview.get_selection(), Gtk.SelectionMode.MULTIPLE)
-
-            treeview.connect("button-press-event", self.on_button_press, disc)
-            treeview.connect("key-press-event", self.on_key_press, disc)
-
-            scroll.add(treeview)
+            scroll = self.get_tracklist(disc)
             tabbed.append_page(scroll, None)
             tabbed.set_tab_label_text(scroll, os.path.basename(disc))
 
-        vbox.pack_start(tabbed, True, True, 0)
+        return tabbed
 
-        self.show_all()
+    def get_tracklist(self, disc):
+        # scrolled area for the songs
+        scroll = Gtk.ScrolledWindow()
+        scroll.set_border_width(0)
+        scroll.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+
+        # set up the song treeview
+        liststore = Gtk.ListStore(str)
+        songs = os.listdir(music + disc)
+        songs.sort()
+        for song in songs:
+            if song.endswith(("mp3", "mpc", "ogg", "wma", "m4a", "mp4", "flac")):
+                liststore.append([song])
+        treeview = Gtk.TreeView(model=liststore)
+        renderer = Gtk.CellRendererText()
+        # option is PangoEllipsizeMode numbered 0,1,2,3 for none, start,
+        # middle, end (True becomes 1=start)
+        renderer.set_property("ellipsize", 3)
+        column = Gtk.TreeViewColumn("Track", renderer, text=0)
+        treeview.append_column(column)
+        treeview.set_property("headers-visible", False)
+        # disable search grabbing keyboard input
+        treeview.set_enable_search(False)
+
+        # allow selection of multiple rows. get_selection won't work
+        # now so use get_selected_rows instead (on selection object)
+        Gtk.TreeSelection.set_mode(
+            treeview.get_selection(), Gtk.SelectionMode.MULTIPLE)
+
+        treeview.connect("button-press-event", self.on_button_press, disc)
+        treeview.connect("key-press-event", self.on_key_press, disc)
+
+        scroll.add(treeview)
+
+        return scroll
 
     def on_button_press(self, widget, event, disc):
         # print(event.type)
