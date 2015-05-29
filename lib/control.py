e5afed853bafe98f21837ac7a564452a644b4b59
judgedreads <pearce@millerdedmon.com>
2015-05-28 21:01:17 -0700

    update control icons and code

--- lib/control.py
+++ lib/control.py
@@ -15,28 +15,40 @@ class Control(Gtk.Box):
             "/opt/ricochet/images/default_album.jpg", 256, 256)
         self.player.image.set_from_pixbuf(self.player.pixbuf)
         self.player.image.show()
+        # FIXME: the multi disc albums look in the directory containing
+        # the songs rather than the toplevel album directory.
 
         Gtk.Box.__init__(self, orientation=1)
         self.pack_start(self.player.image, False, False, 0)
 
         button_box = Gtk.Box()
         button1 = Gtk.Button()
-        pixbuf1 = GdkPixbuf.Pixbuf.new_from_file('images/prev.png')
+        pixbuf1 = GdkPixbuf.Pixbuf.new_from_file_at_size(
+            'images/prev.png', 20, 20)
         image1 = Gtk.Image().new_from_pixbuf(pixbuf1)
         button1.add(image1)
         button2 = Gtk.Button()
-        pixbuf2 = GdkPixbuf.Pixbuf.new_from_file('images/next.png')
+        pixbuf2 = GdkPixbuf.Pixbuf.new_from_file_at_size(
+            'images/next.png', 20, 20)
         image2 = Gtk.Image().new_from_pixbuf(pixbuf2)
         button2.add(image2)
         button3 = Gtk.Button()
-        pixbuf3 = GdkPixbuf.Pixbuf.new_from_file('images/play.png')
+        pixbuf3 = GdkPixbuf.Pixbuf.new_from_file_at_size(
+            'images/play.png', 20, 20)
         image3 = Gtk.Image().new_from_pixbuf(pixbuf3)
         button3.add(image3)
+        button4 = Gtk.Button()
+        pixbuf4 = GdkPixbuf.Pixbuf.new_from_file_at_size(
+            'images/stop.png', 20, 20)
+        image4 = Gtk.Image().new_from_pixbuf(pixbuf4)
+        button4.add(image4)
         button1.connect("clicked", self.player.skip_prev)
         button2.connect("clicked", self.player.skip_next)
         button3.connect("clicked", self.toggle, button3)
+        button4.connect("clicked", self.player.stop)
         button_box.pack_start(button1, True, True, 0)
         button_box.pack_start(button3, True, True, 0)
+        button_box.pack_start(button4, True, True, 0)
         button_box.pack_start(button2, True, True, 0)
 
         self.pack_start(button_box, False, False, 0)
