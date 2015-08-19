29a8588973407e32d9967d389842d8a720cdf94e
judgedreads <pearce@millerdedmon.com>
2015-08-18 23:54:27 -0700

    initial separation of front and back end

--- lib/player/control.py
+++ lib/player/control.py
@@ -1,30 +1,39 @@
 from gi.repository import Gtk, GdkPixbuf
+import os
+from ..notifications import Notifier
+
+# TODO: Make this be the connection between gui and backend by using the backend
+# as an API. Will need API endpoints to skip, toggle, set states etc.
 
 
 class Control(Gtk.Box):
 
-    '''The side panel for the playlist and playback controls'''
+    '''
+    The GUI component of the player. All interaction with the backend playback
+    of music is done through this module, including handling of signals.
+    '''
 
     def __init__(self, player, server):
         self.player = player
+        self.player.bus.connect('message::eos', self.on_eos)
         self.server = server
 
-        self.player.image = Gtk.Image()
-        self.player.pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(
+        self.image = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(
             "/opt/ricochet/images/default_album.png", 256, 256)
-        self.player.image.set_from_pixbuf(self.player.pixbuf)
-        self.player.image.show()
+        self.image.set_from_pixbuf(pixbuf)
+        self.image.show()
         # FIXME: the multi disc albums look in the directory containing
         # the songs rather than the toplevel album directory.
 
         Gtk.Box.__init__(self, orientation=1)
-        self.pack_start(self.player.image, False, False, 0)
+        self.pack_start(self.image, False, False, 0)
 
         buttons = [
-            ('\u25AE\u25C0', self.player.skip_prev),
-            ('\u25B6\u25AE\u25AE', self.player.toggle),
-            ('\u25FC', self.player.stop),
-            ('\u25B6\u25AE', self.player.skip_next)
+            ('\u25AE\u25C0', self.skip_prev),
+            ('\u25B6\u25AE\u25AE', self.toggle),
+            ('\u25FC', self.stop),
+            ('\u25B6\u25AE', self.skip_next)
         ]
         button_box = Gtk.Box()
         for label, method in buttons:
@@ -39,5 +48,93 @@ class Control(Gtk.Box):
         scroll.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         self.pack_start(scroll, True, True, 0)
 
-        scroll.add(self.player.treeview)
+        # create playlist tree widget
+        self.liststore = Gtk.ListStore(str)
+        self.treeview = Gtk.TreeView()
+        self.treeview.set_enable_search(False)
+        selection = self.treeview.get_selection()
+        selection.set_mode(Gtk.SelectionMode.MULTIPLE)
+
+        self.change_playlist()
+
+        renderer = Gtk.CellRendererText()
+        renderer.set_property("ellipsize", 3)
+        column = Gtk.TreeViewColumn("Playlist", renderer, text=0)
+        self.treeview.append_column(column)
+
+        self.treeview.connect("row-activated", self.on_activate)
+        self.treeview.connect("key_press_event", self.on_key_press)
+
+        scroll.add(self.treeview)
         self.show_all()
+
+    def on_eos(self, bus, msg):
+        '''callback for when the end of a song is reached'''
+        self.skip_next()
+
+    def change_playlist(self, widget=None):
+        '''handle playlist changes'''
+        self.liststore.clear()
+        for song in self.player.change_playlist():
+            self.liststore.append([song])
+        self.treeview.set_model(self.liststore)
+
+    def on_key_press(self, widget, event):
+        if event.hardware_keycode == 119:
+            selection = widget.get_selection()
+            songs = selection.get_selected_rows()[1]
+            for song in songs:
+                for track in self.player.playlist:
+                    if self.liststore[song][0] in track:
+                        self.player.playlist.remove(track)
+            self.change_playlist(None)
+
+    def on_activate(self, tree, path, column):
+        '''callback for activation on playlist tree'''
+        song = self.liststore[path][0]
+        song = song.replace('\u25B6 ', '')
+        song = song.replace('\u25AE\u25AE ', '')
+        self.player.select_song(song)
+        self.notify()
+        self.update_image()
+        self.change_playlist()
+
+    def skip_prev(self, widget=None):
+        self.player.skip_prev()
+        self.notify()
+        self.update_image()
+        self.change_playlist()
+
+    def skip_next(self, widget=None):
+        self.player.skip_next()
+        self.notify()
+        self.update_image()
+        self.change_playlist()
+
+    def toggle(self, widget=None):
+        '''toggle play state'''
+        self.player.toggle()
+        self.update_image()
+        self.change_playlist()
+
+    def stop(self, widget=None):
+        self.player.stop()
+        self.update_image()
+        self.change_playlist()
+
+    def update_image(self):
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(
+            "/opt/ricochet/images/default_album.png", 256, 256)
+        if self.player.playlist:
+            segs = self.player.playlist[self.player.track - 1].split('/')
+            path = '/'.join(segs[2:-1]) + '/cover.jpg'
+            if os.path.exists(path):
+                pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(
+                    path, 256, 256)
+        self.image.set_from_pixbuf(pixbuf)
+        self.image.show()
+
+    def notify(self):
+        n = Notifier(self)
+        if self.settings['notifications'] == "True":
+            n.notify(self.player.track - 1)
--- lib/player/gstapi.py
+++ lib/player/gstapi.py
@@ -1,7 +1,5 @@
-from gi.repository import Gst, Gtk, GdkPixbuf
+from gi.repository import Gst
 import os
-
-from ..notifications import Notifier
 
 
 class Player(object):
@@ -24,55 +22,9 @@ class Player(object):
         self.pipeline = Gst.Pipeline()
         self.bus = self.pipeline.get_bus()
         self.bus.add_signal_watch()
-        self.bus.connect('message::eos', self.on_eos)
         # about-to-finish signal for gapless
         self.playbin = Gst.ElementFactory.make('playbin', None)
         self.pipeline.add(self.playbin)
-
-        # create playlist tree widget
-        self.liststore = Gtk.ListStore(str)
-        self.treeview = Gtk.TreeView()
-        self.treeview.set_enable_search(False)
-        selection = self.treeview.get_selection()
-        selection.set_mode(Gtk.SelectionMode.MULTIPLE)
-
-        self.change_playlist()
-
-        renderer = Gtk.CellRendererText()
-        renderer.set_property("ellipsize", 3)
-        column = Gtk.TreeViewColumn("Playlist", renderer, text=0)
-        self.treeview.append_column(column)
-
-        self.treeview.connect("row-activated", self.on_activate)
-        self.treeview.connect("key_press_event", self.on_key_press)
-
-    def on_activate(self, tree, path, column):
-        '''callback for activation on playlist tree'''
-        song = self.liststore[path][0]
-        song = song.replace('\u25B6 ', '')
-        song = song.replace('\u25AE\u25AE ', '')
-        for num, name in enumerate(self.playlist):
-            name = os.path.basename(name)
-            if name.split('.')[0] == song:
-                self.pipeline.set_state(Gst.State.NULL)
-                self.playbin.set_property('uri', self.playlist[num])
-                self.track = num + 1
-                break
-        self.pipeline.set_state(Gst.State.PLAYING)
-        self.current_state = "PLAYING"
-        self.notify()
-        self.update_image()
-        self.change_playlist()
-
-    def on_key_press(self, widget, event):
-        if event.hardware_keycode == 119:
-            selection = widget.get_selection()
-            songs = selection.get_selected_rows()[1]
-            for song in songs:
-                for track in self.playlist:
-                    if self.liststore[song][0] in track:
-                        self.playlist.remove(track)
-            self.change_playlist(None)
 
     def get_info(self, widget, data):
         i = self.track
@@ -96,21 +48,8 @@ class Player(object):
             artist = self.playlist[i - 1].split('/')[-3]
             return artist
 
-    def update_image(self):
-        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(
-            "/opt/ricochet/images/default_album.png", 256, 256)
-        if self.playlist:
-            segs = self.playlist[self.track - 1].split('/')
-            path = '/'.join(segs[2:-1]) + '/cover.jpg'
-            if os.path.exists(path):
-                pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(
-                    path, 256, 256)
-        self.image.set_from_pixbuf(pixbuf)
-        self.image.show()
-
     def change_playlist(self, widget=None):
         '''handle playlist changes'''
-        self.liststore.clear()
         for i, item in enumerate(self.playlist):
             segs = item.split('/')[-1].split('.')[:-1]
             song = '.'.join(segs)
@@ -119,8 +58,7 @@ class Player(object):
                     song = '\u25B6 ' + song
                 elif self.current_state == 'PAUSED':
                     song = '\u25AE\u25AE ' + song
-            self.liststore.append([song])
-        self.treeview.set_model(self.liststore)
+            yield song
 
     def toggle(self, widget=None):
         '''toggle play state'''
@@ -136,9 +74,6 @@ class Player(object):
             self.playbin.set_property('uri', self.playlist[0])
             self.pipeline.set_state(Gst.State.PLAYING)
             self.current_state = "PLAYING"
-            self.update_image()
-            self.notify()
-        self.change_playlist()
         print(self.pipeline.get_state(Gst.State.NULL))
 
     def play(self, widget=None, files=None):
@@ -179,6 +114,21 @@ class Player(object):
 
         self.change_playlist()
 
+    def select_song(self, song):
+        '''
+        Temporary function for handling selecting song from playlist. May become
+        permanent or refactored.
+        '''
+        for num, name in enumerate(self.player.playlist):
+            name = os.path.basename(name)
+            if name.split('.')[0] == song:
+                self.pipeline.set_state(Gst.State.NULL)
+                self.playbin.set_property('uri', self.player.playlist[num])
+                self.track = num + 1
+                break
+        self.pipeline.set_state(Gst.State.PLAYING)
+        self.current_state = "PLAYING"
+
     def quit(self, widget, event):
         self.pipeline.set_state(Gst.State.NULL)
 
@@ -189,12 +139,9 @@ class Player(object):
         self.track = 1
         if clear_playlist is True:
             self.playlist = []
-        self.change_playlist()
-        self.update_image()
         print(self.pipeline.get_state(Gst.State.NULL))
 
     def skip_next(self, widget=None):
-        print('skipping')
         self.pipeline.set_state(Gst.State.NULL)
         i = self.track
         if i < len(self.playlist):
@@ -202,9 +149,6 @@ class Player(object):
             self.pipeline.set_state(Gst.State.PLAYING)
             self.current_state = "PLAYING"
             self.track += 1
-            self.notify()
-            self.update_image()
-            self.change_playlist()
         else:
             self.stop(clear_playlist=False)
 
@@ -217,27 +161,3 @@ class Player(object):
         self.pipeline.set_state(Gst.State.PLAYING)
         self.current_state = "PLAYING"
         self.track -= 1
-        self.notify()
-        self.update_image()
-        self.change_playlist()
-
-    def notify(self):
-        n = Notifier(self)
-        if self.settings['notifications'] == "True":
-            n.notify(self.track - 1)
-
-    def on_eos(self, bus, msg):
-        '''callback for when the end of a song is reached'''
-        # TODO:use self.skip_next(), investigate a 'nearly finished'
-        # signal to allow more gapless playback or try not setting NULL?
-        i = self.track
-        self.pipeline.set_state(Gst.State.NULL)
-        if i < len(self.playlist):
-            self.playbin.set_property('uri', self.playlist[i])
-            self.pipeline.set_state(Gst.State.PLAYING)
-            self.track += 1
-            self.notify()
-            self.update_image()
-            self.change_playlist()
-        else:
-            self.stop(clear_playlist=False)
